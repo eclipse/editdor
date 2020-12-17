c2a5d4df6243c1c7a6ab9a1f53a902b7eb2ffb1c
Felix Paulini <felix.paulini@evosoft.com>
2020-12-17 16:08:15 +0100

    Permalink repaired and some restructuring of the buttons

--- src/components/TDViewer/TDViewer.jsx
+++ src/components/TDViewer/TDViewer.jsx
@@ -295,12 +295,12 @@ export default function TDViewer() {
 
             <div className="flex justify-start items-end pt-8 pb-4">
                 <div className="flex flex-row justify-start items-end flex-grow">
-                    <div className="text-2xl text-white mr-4">Properties</div>
+                    <div className="text-2xl text-white mr-4 flex-grow">Properties</div>
                     <button className="text-white bg-blue-500 cursor-pointer rounded-md p-2" onClick={() => sortKeysInObject('properties')}>
                         {sortedIcon()}
                     </button>
                     <div className="relative text-gray-600">
-                        <input type="search" autoComplete="on" className="flex-grow px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={search} placeholder="Search Properties" aria-label="Search through all Properties" />
+                        <input type="search" autoComplete="on" className="px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={search} placeholder="Search Properties" aria-label="Search through all Properties" />
                         <button type="submit" className="cursor-default absolute right-0 top-0 mt-1 mr-6">
                             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{ enableBackground: 'new 0 0 56.966 56.966' }} space="preserve" width="512px" height="512px">
                                 <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
@@ -319,12 +319,12 @@ export default function TDViewer() {
 
             <div className="flex justify-between items-end pt-8 pb-4">
                 <div className="flex flex-row justify-start items-end flex-grow">
-                    <div className="text-2xl text-white pl-1 mr-4 ">Actions</div>
+                    <div className="text-2xl text-white pl-1 mr-4 flex-grow">Actions</div>
                     <button className="text-white bg-blue-500 cursor-pointer rounded-md p-2" onClick={() => sortKeysInObject('actions')}>
                         {sortedIcon()}
                     </button>
                     <div className="relative text-gray-600">
-                        <input type="search" autoComplete="on" className="flex-grow px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={searchActions} placeholder="Search Actions" aria-label="Search through all Properties" />
+                        <input type="search" autoComplete="on" className="px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={searchActions} placeholder="Search Actions" aria-label="Search through all Properties" />
                         <button type="submit" disabled className="cursor-default absolute right-0 top-0 mt-1 mr-6">
                             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{ enableBackground: 'new 0 0 56.966 56.966' }} space="preserve" width="512px" height="512px">
                                 <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
@@ -343,12 +343,12 @@ export default function TDViewer() {
 
             <div className="flex justify-between items-end pt-8 pb-4">
                 <div className="flex flex-row justify-start items-end flex-grow">
-                    <div className="text-2xl text-white mr-4">Events</div>
+                    <div className="text-2xl text-white mr-4 flex-grow">Events</div>
                     <button className="text-white bg-blue-500 cursor-pointer rounded-md p-2" onClick={() => sortKeysInObject('events')}>
                         {sortedIcon()}
                     </button>
                     <div className="relative text-gray-600">
-                        <input type="search" autoComplete="on" className="flex-grow px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={searchEvents} placeholder="Search Events" aria-label="Search through all Properties" />
+                        <input type="search" autoComplete="on" className="px-5 pr-10 ml-4 mr-4 place-self-center rounded-full text-sm focus:outline-none" onKeyUp={searchEvents} placeholder="Search Events" aria-label="Search through all Properties" />
                         <button disabled type="submit" className="cursor-default absolute right-0 top-0 mt-1 mr-6">
                             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{ enableBackground: 'new 0 0 56.966 56.966' }} space="preserve" width="512px" height="512px">
                                 <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
