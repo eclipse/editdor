a02fe8106e5192cd99f711bf12a7a6bb8535f5b5
sebastiankb <sebastian.kaebisch@siemens.com>
2021-02-25 15:04:30 +0000

    Deploying to gh-pages from  @ 63ce7cb48feea1671aac265fab16f119f37e1e7f 🚀

---
+++ precache-manifest.338d0d3313ff1bec0ccb141fe1dad337.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6767435b317c49da4d15f579dc24bec8",
+    "url": "/editdor/editor.worker.js"
+  },
+  {
+    "revision": "e50f28af7a00408231993c0ec528356d",
+    "url": "/editdor/index.html"
+  },
+  {
+    "revision": "a9ecc5a1fe34bbec36c70bbcd7ac13dc",
+    "url": "/editdor/json.worker.js"
+  },
+  {
+    "revision": "9d2eddf718340f6f92c6",
+    "url": "/editdor/static/css/2.917396f4.chunk.css"
+  },
+  {
+    "revision": "3a7f672e4a60eba08567",
+    "url": "/editdor/static/css/main.29feb616.chunk.css"
+  },
+  {
+    "revision": "9d2eddf718340f6f92c6",
+    "url": "/editdor/static/js/2.3e3b621d.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/editdor/static/js/2.3e3b621d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49eff3e536087e3ce65d",
+    "url": "/editdor/static/js/3.564c6c6f.chunk.js"
+  },
+  {
+    "revision": "3a7f672e4a60eba08567",
+    "url": "/editdor/static/js/main.212a7e73.chunk.js"
+  },
+  {
+    "revision": "1c244360960606d75453",
+    "url": "/editdor/static/js/runtime-main.70f5b234.js"
+  },
+  {
+    "revision": "89768410a865a331824f32060a494e2e",
+    "url": "/editdor/static/media/WoT.89768410.png"
+  },
+  {
+    "revision": "8e177a557add1e5add06a1b5e20465ce",
+    "url": "/editdor/static/media/codicon.8e177a55.ttf"
+  },
+  {
+    "revision": "6cac600de51081ebba75ad0cfd925f4c",
+    "url": "/editdor/static/media/editdor.6cac600d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/editdor/precache-manifest.6c1e3397c52bdca2b82c5e0ae1903a35.js"
+  "/editdor/precache-manifest.338d0d3313ff1bec0ccb141fe1dad337.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/editdor/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
