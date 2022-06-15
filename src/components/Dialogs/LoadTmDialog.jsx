f3376ca96080e51b87359c2628c07c735f026836
ivan <idimitryz@gmail.com>
2022-06-15 20:27:11 +0200

    feat: improve load thing model modal

--- src/components/Dialogs/LoadTmDialog.jsx
+++ src/components/Dialogs/LoadTmDialog.jsx
@@ -19,7 +19,8 @@ import React, {
 import ReactDOM from "react-dom";
 import { DialogTemplate } from "./DialogTemplate";
 import ediTDorContext from "../../context/ediTDorContext";
-import { ChevronDown } from "react-feather";
+import { ChevronDown, Search } from "react-feather";
+
 //TODO move to .env
 const emporioUrl = `http://localhost:3003`;
 
@@ -33,20 +34,6 @@ export const LoadTmDialog = forwardRef((props, ref) => {
     []
   );
 
-  useEffect(() => {
-    const getThingModels = async () => {
-      const tmsFromServer = await fetchThingModels();
-      setThingModels(tmsFromServer);
-    };
-    getThingModels();
-  }, []);
-
-  const fetchThingModels = async () => {
-    const res = await fetch(`${emporioUrl}/models`);
-    const data = await res.json();
-    return data;
-  };
-
   useImperativeHandle(ref, () => {
     return {
       openModal: () => open(),
@@ -54,15 +41,52 @@ export const LoadTmDialog = forwardRef((props, ref) => {
     };
   });
 
-  const open = () => {
+  const open = async () => {
+    setThingModels(await fetchThingModels());
     setDisplay(true);
   };
 
   const close = () => {
     setDisplay(false);
   };
+  const setSelectedThingModel = (index) => {
+    setThingModels(
+      thingModels.map((thing, thingIndex) => {
+        if (thingIndex === index) thing.selected = true;
+        else thing.selected = false;
+        return thing;
+      })
+    );
+  };
 
-  const content = buildForm(thingModels, setChoosenModel);
+  const fetchThingModels = async () => {
+    const res = await fetch(`${emporioUrl}/models`);
+    const data = await res.json();
+    return data;
+  };
+
+  const searchThingModels = async () => {
+    const attribute =
+      document.getElementById("search-option").value;
+    const searchText =
+      document.getElementById("search-id").value;
+    if (searchText === "") {
+      return setThingModels(await fetchThingModels());
+    }
+    console.log(attribute);
+    console.log(searchText);
+    const res = await fetch(
+      `${emporioUrl}/models?${attribute}=${searchText}`
+    );
+    setThingModels(await res.json());
+  };
+
+  const content = buildForm(
+    thingModels,
+    setChoosenModel,
+    setSelectedThingModel,
+    searchThingModels
+  );
 
   if (display) {
     return ReactDOM.createPortal(
@@ -70,10 +94,10 @@ export const LoadTmDialog = forwardRef((props, ref) => {
         onCancel={close}
         onSubmit={() => {
           let thingModel = choosenModel;
-          let linkedTd = {};
-          linkedTd[thingModel["title"]] = thingModel;
+          let linkedModel = {};
+          linkedModel[thingModel["title"]] = thingModel;
           context.updateLinkedTd(undefined);
-          context.addLinkedTd(linkedTd);
+          context.addLinkedTd(linkedModel);
           context.updateShowConvertBtn(true);
           context.updateOfflineTD(
             JSON.stringify(thingModel, null, "\t"),
@@ -95,49 +119,101 @@ export const LoadTmDialog = forwardRef((props, ref) => {
   return null;
 });
 
-const buildForm = (thingModels, setChoosenModel) => {
+const buildForm = (
+  thingModels,
+  setChoosenModel,
+  setSelectedThingModel,
+  searchThingModels
+) => {
   return (
     <>
-      <div className="relative"></div>
+      <div className="flex">
+        <div className="relative w-1/4">
+          <select
+            className="block appearance-none w-full bg-gray-600 border-2 border-gray-600 text-white py-3 px-4 pr-8 rounded leading-tight focus:border-blue-500 focus:outline-none"
+            id="search-option"
+          >
+            <option selected>title</option>
+            <option>description</option>
+            <option>type</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <ChevronDown color="#cacaca"></ChevronDown>
+          </div>
+        </div>
+        <input
+          name="search-id"
+          id="search-id"
+          className="w-7/10 border-gray-600 bg-gray-600 p-2 sm:text-sm border-2 text-white rounded-md focus:outline-none focus:border-blue-500"
+          placeholder="Search Thing Models"
+          type="search"
+        />
+
+        <button
+          type="submit"
+          class="text-white bg-blue-500 p-2 rounded-md"
+          onClick={() => {
+            searchThingModels();
+          }}
+        >
+          Search
+        </button>
+      </div>
       {thingModels.map((thingModel, index) => (
-        <ThingModel key={index} thingModel={thingModel} setChoosenModel={setChoosenModel}/>
+        <ThingModel
+          key={index}
+          index={index}
+          thingModel={thingModel}
+          setChoosenModel={setChoosenModel}
+          setSelectedThingModel={setSelectedThingModel}
+        />
       ))}
     </>
   );
 };
 
-const ThingModel = ({ thingModel, setChoosenModel }) => {
+const ThingModel = ({
+  thingModel,
+  index,
+  setChoosenModel,
+  setSelectedThingModel,
+}) => {
+  const types = formatThingModeltypes(thingModel["@type"]);
   return (
     <div
-      className="thingModel"
-      onDoubleClick={() => {
-        console.log(`choosen model is ${thingModel.title}`);
+      className={`thingModel 
+        my-1
+        p-1
+        bg-gray-600 
+        border-2 
+        rounded w-full 
+        text-white
+        ${
+          thingModel.selected
+            ? "border-blue-500"
+            : "border-gray-600"
+        }`}
+      onClick={() => {
+        setSelectedThingModel(index);
         setChoosenModel(thingModel);
       }}
     >
-      <h3>
-        {thingModel.title} {thingModel["@type"]}
-      </h3>
-      <p>{thingModel.description}</p>
+      <div className="relative">
+        <h3 className="px-2">{thingModel.title}</h3>
+        <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-400">
+          {types ? types.map((type) => <p>{type}</p>) : ""}
+        </div>
+      </div>
+      <p className="text-gray-300 py-1 pl-1 my-1">
+        {thingModel.description}
+      </p>
     </div>
   );
 };
 
-const formField = (
-  label,
-  placeholder,
-  id,
-  type,
-  autoFocus
-) => {
+const formField = (placeholder, id, type, autoFocus) => {
   return (
     <div key={id} className="py-1">
-      <label
-        htmlFor={id}
-        className="text-sm text-gray-400 font-medium pl-2"
-      >
-        {label}:
-      </label>
       <input
         name={id}
         id={id}
@@ -148,4 +224,12 @@ const formField = (
       />
     </div>
   );
+};
+
+const formatThingModeltypes = (type) => {
+  if (Array.isArray(type)) {
+    return type.filter(
+      (element) => element !== "tm:ThingModel"
+    );
+  }
 };
