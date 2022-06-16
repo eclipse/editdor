79f397a0967b391500c0a74c9010a3a199cde6f8
ivan <idimitryz@gmail.com>
2022-06-16 12:59:02 +0200

    feat: add pagination

--- src/components/Dialogs/LoadTmDialog.jsx
+++ src/components/Dialogs/LoadTmDialog.jsx
@@ -12,14 +12,17 @@
  ********************************************************************************/
 import React, {
   forwardRef,
-  useEffect,
   useImperativeHandle,
   useContext,
 } from "react";
 import ReactDOM from "react-dom";
 import { DialogTemplate } from "./DialogTemplate";
 import ediTDorContext from "../../context/ediTDorContext";
-import { ChevronDown, Search } from "react-feather";
+import {
+  ChevronDown,
+  ChevronRight,
+  ChevronLeft,
+} from "react-feather";
 
 //TODO move to .env
 const emporioUrl = `http://localhost:3003`;
@@ -33,6 +36,11 @@ export const LoadTmDialog = forwardRef((props, ref) => {
   const [choosenModel, setChoosenModel] = React.useState(
     []
   );
+  const [pagination, setPagination] = React.useState({
+    currentPage: 0,
+    loading: null,
+    thingModelsPerPage: 5,
+  });
 
   useImperativeHandle(ref, () => {
     return {
@@ -43,6 +51,7 @@ export const LoadTmDialog = forwardRef((props, ref) => {
 
   const open = async () => {
     setThingModels(await fetchThingModels());
+    setPagination({ ...pagination, loading: false });
     setDisplay(true);
   };
 
@@ -59,33 +68,66 @@ export const LoadTmDialog = forwardRef((props, ref) => {
     );
   };
 
-  const fetchThingModels = async () => {
-    const res = await fetch(`${emporioUrl}/models`);
+  const fetchThingModels = async (
+    page = 0,
+    attribute = false,
+    searchText
+  ) => {
+    const offset = pagination.thingModelsPerPage * page;
+    let url = `${emporioUrl}/models?limit=${pagination.thingModelsPerPage}&offset=${offset}`;
+    if (attribute) url += `&${attribute}=${searchText}`;
+    const res = await fetch(url);
     const data = await res.json();
     return data;
   };
 
-  const searchThingModels = async () => {
-    const attribute =
-      document.getElementById("search-option").value;
+  const paginate = async (direction) => {
+    const page =
+      direction === "right"
+        ? pagination.currentPage + 1
+        : pagination.currentPage - 1;
     const searchText =
       document.getElementById("search-id").value;
-    if (searchText === "") {
-      return setThingModels(await fetchThingModels());
-    }
-    console.log(attribute);
-    console.log(searchText);
-    const res = await fetch(
-      `${emporioUrl}/models?${attribute}=${searchText}`
-    );
-    setThingModels(await res.json());
+    const attribute =
+      document.getElementById("search-option").value;
+    if (page < 0) return;
+    const thingModels =
+      searchText === ""
+        ? await fetchThingModels(page)
+        : await fetchThingModels(
+            page,
+            attribute,
+            searchText
+          );
+    if (thingModels.length <= 0) return;
+    setThingModels(thingModels);
+    setPagination({ ...pagination, currentPage: page });
+  };
+
+  const searchThingModels = async (page = 0) => {
+    const searchText =
+      document.getElementById("search-id").value;
+    const attribute =
+      document.getElementById("search-option").value;
+    setPagination({ ...pagination, currentPage: 0 });
+    return searchText === ""
+      ? setThingModels(await fetchThingModels(page))
+      : setThingModels(
+          await fetchThingModels(
+            page,
+            attribute,
+            searchText
+          )
+        );
   };
 
   const content = buildForm(
     thingModels,
+    pagination.currentPage,
     setChoosenModel,
     setSelectedThingModel,
-    searchThingModels
+    searchThingModels,
+    paginate
   );
 
   if (display) {
@@ -93,14 +135,13 @@ export const LoadTmDialog = forwardRef((props, ref) => {
       <DialogTemplate
         onCancel={close}
         onSubmit={() => {
-          let thingModel = choosenModel;
           let linkedModel = {};
-          linkedModel[thingModel["title"]] = thingModel;
+          linkedModel[choosenModel["title"]] = choosenModel;
           context.updateLinkedTd(undefined);
           context.addLinkedTd(linkedModel);
           context.updateShowConvertBtn(true);
           context.updateOfflineTD(
-            JSON.stringify(thingModel, null, "\t"),
+            JSON.stringify(choosenModel, null, "\t"),
             "AppHeader"
           );
           close();
@@ -121,9 +162,11 @@ export const LoadTmDialog = forwardRef((props, ref) => {
 
 const buildForm = (
   thingModels,
+  page,
   setChoosenModel,
   setSelectedThingModel,
-  searchThingModels
+  searchThingModels,
+  paginate
 ) => {
   return (
     <>
@@ -168,6 +211,25 @@ const buildForm = (
           setSelectedThingModel={setSelectedThingModel}
         />
       ))}
+
+      <div className="flex justify-center pt-4 p-2">
+        <button
+          className="text-white bg-gray-500 p-2 mr-1 rounded-md"
+          onClick={() => paginate("left")}
+        >
+          <ChevronLeft color="#cacaca"></ChevronLeft>
+        </button>
+        <span className="text-white bg-gray-500 p-2 mr-1 rounded-md">
+          {" "}
+          {page + 1}{" "}
+        </span>
+        <button
+          className="text-white bg-gray-500 p-2 mr-1 rounded-md"
+          onClick={() => paginate("right")}
+        >
+          <ChevronRight color="#cacaca"></ChevronRight>
+        </button>
+      </div>
     </>
   );
 };
@@ -207,21 +269,6 @@ const ThingModel = ({
       <p className="text-gray-300 py-1 pl-1 my-1">
         {thingModel.description}
       </p>
-    </div>
-  );
-};
-
-const formField = (placeholder, id, type, autoFocus) => {
-  return (
-    <div key={id} className="py-1">
-      <input
-        name={id}
-        id={id}
-        className="border-gray-600 bg-gray-600 w-full p-2 sm:text-sm border-2 text-white rounded-md focus:outline-none focus:border-blue-500"
-        placeholder={placeholder}
-        type={type}
-        autoFocus={autoFocus === "autoFocus"}
-      />
     </div>
   );
 };
