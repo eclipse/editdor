3c684df638fbcfcc1617e33b1389a987e4421930
Ricardo M G da Silva <rephyrus0877@protonmail.com>
2025-04-03 15:50:10 +0100

    style enforce prettier configs
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>

--- src/components/TDViewer/components/SearchBar.jsx
+++ src/components/TDViewer/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2018 - 2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2018 - 2022 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -14,23 +14,18 @@ import React from "react";
 import { Search } from "react-feather";
 
 export const SearchBar = (props) => {
-  return (
-    <div className="relative text-gray-600">
-      <input
-        type="search"
-        autoComplete="on"
-        className="caret-gray bg-gray-600 text-white px-5 pr-10 place-self-center rounded-lg text-sm focus:outline-none h-9"
-        onKeyUp={props.onKeyUp}
-        placeholder={props.placeholder}
-        aria-label={props.ariaLabel}
-      />
-      <div
-        disabled
-        type="submit"
-        className="cursor-default absolute right-0 top-0 mt-1 mr-4"
-      >
-        <Search color="#2c2c2e"></Search>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="relative text-gray-600">
+            <input type="search"
+                autoComplete="on"
+                className="caret-gray bg-gray-600 text-white px-5 pr-10 place-self-center rounded-lg text-sm focus:outline-none h-9"
+                onKeyUp={props.onKeyUp}
+                placeholder={props.placeholder}
+                aria-label={props.ariaLabel}
+            />
+            <div disabled type="submit" className="cursor-default absolute right-0 top-0 mt-1 mr-4">
+                <Search color="#2c2c2e"></Search>
+            </div>
+        </div>
+    );
+}
