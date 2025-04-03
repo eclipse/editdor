7fc0e892233e0794f2cf4e26c71d5d29120aa52d
Ricardo M G da Silva <rephyrus0877@protonmail.com>
2025-04-03 15:22:29 +0100

    chore enforce prettier config, delete unused ChatDialog.jsx
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>

--- src/share.js
+++ src/share.js
@@ -1,71 +1,105 @@
-/********************************************************************************
- * Copyright (c) 2018 - 2024 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
- *
- * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
- ********************************************************************************/
 import { compress, decompress } from "./external/TdPlayground";
+import * as wotTdService from "./services/thingsApiService";
+import { getTargetUrl } from "./services/targetUrl";
 import { isThingModel } from "./util";
 
 const tdPrefix = "tdjson";
 const tmPrefix = "tmjson";
 
 /**
- *
- * @param {string} td
+ * 
+ * @param {string} td 
  * @returns {string | undefined}
- *
  * @description
  * prepareTdForSharing takes a TD/TM string and tries to compress
- * it for sharing. If the string is no valid JSON, this function will
- * return undefined.
+ * it for sharing.
  */
 export const prepareTdForSharing = (td) => {
-  let tdJSON;
-  try {
-    tdJSON = JSON.parse(td);
-  } catch (e) {
-    console.debug(e);
-    return undefined;
-  }
+    let tdJSON;
+    try {
+        tdJSON = JSON.parse(td);
+    } catch (e) {
+        console.debug(e);
+        return undefined
+    }
 
-  let prefix = tdPrefix;
-  if (isThingModel(tdJSON)) {
-    prefix = tmPrefix;
-  }
+    let prefix = tdPrefix;
+    if (isThingModel(tdJSON)) {
+        prefix = tmPrefix;
+    }
 
-  const compressedTD = compress(prefix.concat(td));
-  return compressedTD;
-};
+    const compressedTD = compress(prefix.concat(td));
+    return compressedTD;
+}
 
 /**
- *
- * @param {string} lzString
+ * 
+ * @param {string} lzString 
  * @returns {object | undefined}
- *
- * @description
- * decompressSharedTd takes a lz string as input, then tries
- * to decompress and parse it as a TD/TM, which it returns.
+ * @description DecompressSharedTd takes a lz string as input, then tries
+ * to decompress and parse it as a TD/TM, which it returns. 
  * If any of these operations fail, this function returns undefined.
  */
 export const decompressSharedTd = (lzString) => {
-  let decompressedTd = decompress(lzString);
-  if (decompressedTd == null || decompressedTd === "") {
+    let decompressedTd = decompress(lzString);
+    if (decompressedTd == null || decompressedTd === "") {
+        return undefined;
+    };
+
+    decompressedTd = decompressedTd.substring(6);
+    try {
+        return JSON.parse(decompressedTd);
+    } catch (e) {
+        console.debug(e);
+    }
+
     return undefined;
-  }
+}
 
-  decompressedTd = decompressedTd.substring(6);
-  try {
-    return JSON.parse(decompressedTd);
-  } catch (e) {
-    console.debug(e);
-  }
+/**
+ * 
+ * @param {string} tdId 
+ * @returns {Object | undefined}
+ * @description Contacts the  proxy WoT native API to fetch a Thing Description.
+ */
+export const fetchTdFromWot = async (tdId) => {
+    const targetUrl = getTargetUrl();
 
-  return undefined;
-};
+    try {
+        let td = await wotTdService.getTD(tdId, targetUrl);
+
+        return td;
+    } catch (error) {
+        console.debug(error);
+    }
+
+    return undefined;
+}
+
+/**
+ * 
+ * @param {string} tdId 
+ * @returns {Object | undefined}
+ * @description Contacts the proxy WoT native API to fetch a Thing Description.
+ */
+export const fetchNbTd = async (tdId) => {
+    const targetUrl = getTargetUrl();
+
+    try {
+        const res = await fetch(`${targetUrl}.things/${tdId}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json", },
+        });
+
+        const payload = await res.json();
+        if (res.status === 400) {
+            throw Error(payload.error);
+        }
+
+        return payload;
+    } catch (error) {
+        console.debug(error);
+    }
+
+    return undefined;
+}
