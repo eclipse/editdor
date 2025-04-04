6107081d9dd1c61c95a6993007ddf0b06c413c21
Ricardo M G da Silva <rephyrus0877@protonmail.com>
2025-04-04 16:40:32 +0100

    docs add copyright headers to source files
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>

--- src/components/Dialogs/SpinnerTemplate.jsx
+++ src/components/Dialogs/SpinnerTemplate.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+/********************************************************************************
+ * Copyright (c) 2018 - 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+
+import React from 'react';
 import ReactDOM from "react-dom";
 
-export const SpinnerTemplate = (_) => {
-  return ReactDOM.createPortal(
-    <div className="bg-transparent-400 absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center text-white">
-      <div className="bg-transparent-400 flex max-h-screen w-1/3 flex-col items-center justify-center p-4">
-        <div className="justify-center overflow-hidden p-2">
-          {showSpinner()}
-        </div>
-      </div>
-    </div>,
-    document.getElementById("modal-root")
-  );
+export const SpinnerTemplate = _ => {
+
+    return ReactDOM.createPortal(
+        <div
+            className="flex bg-transparent-400 w-full h-full absolute top-0 left-0 justify-center items-center z-10 text-white">
+            <div className="bg-transparent-400 w-1/3 flex flex-col justify-center items-center p-4 max-h-screen">
+
+                <div className="justify-center overflow-hidden p-2">
+                    {showSpinner()}
+                </div>
+
+            </div>
+        </div>,
+        document.getElementById("modal-root")
+    );
+
 };
 
-const showSpinner = (_) => {
-  return (
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-};
+const showSpinner = _ => {
+    return (<div className="spinner-container">
+        <div className="loading-spinner">
+        </div>
+    </div>);
+}
--- src/components/TDViewer/components/InteractionSection.jsx
+++ src/components/TDViewer/components/InteractionSection.jsx
@@ -1,14 +1,27 @@
-import React, { useContext, useState } from "react";
-import ediTDorContext from "../../../context/ediTDorContext";
-import { AddActionDialog } from "../../Dialogs/AddActionDialog";
-import { AddEventDialog } from "../../Dialogs/AddEventDialog";
-import { AddPropertyDialog } from "../../Dialogs/AddPropertyDialog";
-import { InfoIconWrapper } from "../../InfoIcon/InfoIcon";
-import { tooltipMapper } from "../../InfoIcon/InfoTooltips";
-import Action from "./Action";
-import Event from "./Event";
-import Property from "./Property";
-import { SearchBar } from "./SearchBar";
+/********************************************************************************
+ * Copyright (c) 2018 - 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+
+import React, { useContext, useState } from 'react';
+import ediTDorContext from '../../../context/ediTDorContext';
+import { AddActionDialog } from '../../Dialogs/AddActionDialog';
+import { AddEventDialog } from '../../Dialogs/AddEventDialog';
+import { AddPropertyDialog } from '../../Dialogs/AddPropertyDialog';
+import { InfoIconWrapper } from '../../InfoIcon/InfoIcon';
+import { tooltipMapper } from '../../InfoIcon/InfoTooltips';
+import Action from './Action';
+import Event from './Event';
+import Property from './Property';
+import { SearchBar } from './SearchBar';
 
 const SORT_ASC = "asc";
 const SORT_DESC = "desc";
